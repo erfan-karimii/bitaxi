1f6593b998399b1818dd51670a141337d07fcc2d
MasoudKheradmandi <104209505+MasoudKheradmandi@users.noreply.github.com>
2024-04-25 04:10:47 +0330

    create Token

--- core/account/views/driver_views.py
+++ core/account/views/driver_views.py
@@ -1,1 +1,12 @@
-from rest_framework.views import APIView
+from rest_framework.views import APIView,Response
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
+
+
+class CustomeObtainAuthToken(ObtainAuthToken):
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({'token': token.key,'user_email':user.email})
