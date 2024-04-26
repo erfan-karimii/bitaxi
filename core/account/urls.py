4ad45bf137670a44832115232fecd68e24f12fc8
erfan-karimi <h410mi3@gmail.com>
2024-04-26 03:49:32 +0330

    remove unnecessery import from urls.py

--- core/account/urls.py
+++ core/account/urls.py
@@ -1,8 +1,5 @@
 from django.urls import path
 
-from rest_framework.authtoken.views import obtain_auth_token
-
-from account.views.customer_views import CustomCustomerAuthToken
 from .views import driver_views
 from account.views.customer_views import CustomCustomerAuthToken , RegisterCustomerView
 
