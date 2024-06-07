b6c7c42d5f085baee2f6b658bd9e067a4c3ea567
erfan-karimi <h410mi3@gmail.com>
2024-06-07 14:04:09 +0330

    Update entrypoint.sh

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -4,9 +4,6 @@ set -e
 
 cd /app/
 
-echo 'Collecting static files...'
-python3 manage-production.py collectstatic --no-input
-
 echo 'Running migrations...'
 python3 manage-production.py migrate --no-input
 
